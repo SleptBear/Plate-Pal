705305819188469a9fabddd06864b1ace0c9e493
Hamilton <hamiltontruong@gmail.com>
2023-03-10 10:26:26 -0800

    Update user model and migrations

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-
+from datetime import datetime
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -10,9 +10,15 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    # Yelp does not use usernames - user will only log in with email
+    # username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False, unique=True)
+    last_name = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    zipcode = db.Column(db.Integer, nullable=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     @property
     def password(self):
@@ -28,6 +34,11 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            # 'username': self.username,
+            'email': self.email,
+            'zipcode': self.zipcode,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
         }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,15 +5,46 @@ from sqlalchemy.sql import text
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        first_name='Demo',
+        last_name='User',
+        email='demo@user.io',
+        zipcode=12345,
+        password='password'
+    )
+    demo1 = User(
+        first_name='Demo1',
+        last_name='User1',
+        email='demo1@user.io',
+        zipcode=67891,
+        password='password'
+    )
+    demo2 = User(
+        first_name='Demo2',
+        last_name='User2',
+        email='demo2@user.io',
+        zipcode=23456,
+        password='password'
+    )
+    demo3 = User(
+        first_name='Demo3',
+        last_name='User3',
+        email='demo2@user.io',
+        zipcode=78912,
+        password='password'
+    )
+    demo4 = User(
+        first_name='Demo4',
+        last_name='User4',
+        email='demo2@user.io',
+        zipcode=34567,
+        password='password'
+    )
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(demo1)
+    db.session.add(demo2)
+    db.session.add(demo3)
+    db.session.add(demo4)
     db.session.commit()
 
 
@@ -25,8 +56,9 @@ def seed_users():
 # it will reset the primary keys for you as well.
 def undo_users():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM users"))
-        
+
     db.session.commit()
