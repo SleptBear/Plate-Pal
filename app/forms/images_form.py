debb068d73108a1fbad3849861768ed1cae6c16d
Hamilton <hamiltontruong@gmail.com>
2023-03-10 21:41:48 -0800

    Complete full and partial CRUDs for businesses, images, reviews

--- app/forms/images_form.py
+++ app/forms/images_form.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired, Email, ValidationError, URL
 from app.models import User
 
 
-def extension_check(form, field):
-    # Checking if user exists
-    url = field.data
-    if not url.endswith('.jpeg', '.jpg', '.png',):
-        raise ValidationError('URL does not end in a valid extension')
+# def extension_check(form, field):
+#     # Checking if user exists
+#     url = field.data
+#     if not url.endswith('.jpeg', '.jpg', '.png',):
+#         raise ValidationError('URL does not end in a valid extension')
 
 class ImageForm(FlaskForm):
-    URL = StringField('email', validators=[DataRequired(), URL(), extension_check])
+    url = StringField('email', validators=[DataRequired(), URL()])
