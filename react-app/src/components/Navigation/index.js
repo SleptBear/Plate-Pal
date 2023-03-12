ea958a252767ad96e7bcc112517045ec788dbeea
Hamilton <hamiltontruong@gmail.com>
2023-03-12 10:16:22 -0700

    Navigation populate add business and review

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -16,7 +16,27 @@ function Navigation({ isLoaded }) {
             <img className="logo-image" src={logo} alt="logo"></img>
           </NavLink>
         </li>
-        {isLoaded && (
+        {isLoaded && sessionUser ? (
+          <div className="nav-right-container">
+            <li>
+              <NavLink exact to="/businesses/new">
+                Add a Business
+              </NavLink>
+            </li>
+            <li>
+              {/* path needs to be updated, 
+                  manual input of 1 
+                  needs to go to page with suggested businesses for user to review
+              */}
+              <NavLink exact to="/businesses/1/reviews/new">
+                Write a Review
+              </NavLink>
+            </li>
+            <li className="nav-right-login">
+              <ProfileButton className="profile-button" user={sessionUser} />
+            </li>
+          </div>
+        ) : (
           <li className="nav-right-login">
             <ProfileButton className="profile-button" user={sessionUser} />
           </li>
