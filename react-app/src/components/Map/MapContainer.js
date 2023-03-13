d2e9d019475300161fede45c68a1f3f88d135115
root <root@DESKTOP-OI90J59.localdomain>
2023-03-13 18:11:08 -0400

    added redirect when click on map marker

--- react-app/src/components/Map/MapContainer.js
+++ react-app/src/components/Map/MapContainer.js
@@ -1,50 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 import { useSelector, useDispatch } from "react-redux";
-import { getAllBusinessesThunk } from '../../store/businesses';
-import os from 'os';
+import { Redirect } from "react-router-dom";
+import { getAllBusinessesThunk } from "../../store/businesses";
 
+const MapContainer = ({ google }) => {
+  let businesses = useSelector((state) => state.businesses.businesses);
+  const dispatch = useDispatch();
+  const [redirectId, setRedirectId] = useState(null);
 
-// const businesses = useSelector((state) => state.businesses.businesses);
+  useEffect(() => {
+    dispatch(getAllBusinessesThunk());
+  }, [dispatch]);
 
+  const handleMarkerClick = (businessId) => {
+    setRedirectId(businessId);
+  };
 
-const MapContainer = ({google}) => {
+  if (!businesses) {
+    return null;
+  }
 
-    let businesses = useSelector((state) => state.businesses.businesses);
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(getAllBusinessesThunk())
-    },[dispatch])
-    // const { google } = this.props;
-    console.log('BUSINESSES')
-    if(!businesses){
-        return null
-    }
-    businesses = Object.values(businesses)
-    console.log(businesses)
+  businesses = Object.values(businesses)
 
-    return(
-
-          <Map
-            google={google}
-            zoom={3}
-            initialCenter={{ lat: 37.7749, lng: -122.4194 }} // San Francisco coordinates
-          >
-            {businesses.map(business => (
-              <Marker
-                key={business.id}
-                title={business.name}
-                name={business.name}
-                position={{ lat: business.lat, lng: business.lng }}
-              />
-            ))}
-          </Map>
-
-    )
-}
-
-
+  return (
+    <>
+      <Map
+        google={google}
+        zoom={3}
+        initialCenter={{ lat: 37.7749, lng: -122.4194 }} // San Francisco coordinates
+      >
+        {businesses.map((business) => (
+          <Marker
+            key={business.id}
+            title={business.name}
+            name={business.name}
+            position={{ lat: business.lat, lng: business.lng }}
+            onClick={() => handleMarkerClick(business.id)}
+          />
+        ))}
+      </Map>
+      {redirectId && <Redirect to={`/businesses/${redirectId}`} />}
+    </>
+  );
+};
 
 export default GoogleApiWrapper({
-  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
 })(MapContainer);
