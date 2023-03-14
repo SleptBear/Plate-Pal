e708a6420d27807cd45fd674b53df10564208e9b
Hamilton <hamiltontruong@gmail.com>
2023-03-13 17:12:34 -0700

    Business search filtering continue

--- react-app/src/components/Business/BusinessSearched/index.js
+++ react-app/src/components/Business/BusinessSearched/index.js
@@ -13,6 +13,13 @@ const BusinessSearched = () => {
   const { searchString } = useParams();
   const [userSearchString, setUserSearchString] = useState("");
 
+  // frontend filtering
+  const [userFiltered, setUserFiltered] = useState(false);
+
+  // price hover
+  const [priceFilter, setPriceFilter] = useState(0);
+  const [hover, setHover] = useState(0);
+
   let searchResult = useSelector((state) => state.businesses.businesses);
   if (searchResult !== null) searchResult = Object.values(searchResult);
 
@@ -23,21 +30,114 @@ const BusinessSearched = () => {
     searchBusinesses();
   }, [dispatch, searchString]);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    setUserFiltered(true);
+
+    // perform frontend filtering of business state with user's input
+    // conditionally render with filter if "userFiltered" is true
+    // eg setFilteredBusinsesses
+    // in jsx setFilteredBusinesses.map(business => ...)
+    
+  };
+
   if (searchResult === null || searchResult.length === 0) return null;
 
   return (
     <div className="business-search-container">
-      <div>
-        Filters
-        <div>Price filter buttons</div>
-        <div>Categories</div>
+      <div className="business-search-filters">
+        <form onSubmit={onSubmit}>
+          <h4>Filters</h4>
+          <br></br>
+          <div className="business-search-price-filter">
+            {[...Array(5)].map((x, index) => {
+              index += 1;
+              return (
+                <button
+                  type="button"
+                  key={index}
+                  className={index <= (hover || priceFilter) ? "on" : "off"}
+                  onClick={() => setPriceFilter(index)}
+                  onMouseEnter={() => setHover(index)}
+                  onMouseLeave={() => setHover(priceFilter)}
+                >
+                  <span className="price-rating">$</span>
+                </button>
+              );
+            })}
+          </div>
+          <br></br>
+          <hr></hr>
+          <br></br>
+          <h4>Categories</h4>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              American
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Californian
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Farm-to-Table
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Molecular Gastronomy
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              French
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Italian
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Seafood
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Southern
+            </label>
+          </div>
+          <div>
+            <label>
+              <input type="checkbox"></input>
+              Steakhouse
+            </label>
+          </div>
+          <br></br>
+          <hr></hr>
+          <br></br>
+          <button>Filter</button>
+        </form>
       </div>
-      <div>
+
+      <div className="business-search-results">
         {searchResult.map((business) => {
           return <BusinessSearchCard business={business} key={business.id} />;
         })}
       </div>
-      <div>
+      <div className="business-search-map">
         <MapPage searchString={searchString} />
       </div>
     </div>
