c215da8b489df2ee8acd512dbbb8c84cdd0c0e29
Murphyn5 <nlimurphy@gmail.com>
2023-03-19 23:01:14 -0700

    changed lat and lng seed data back to decimals

--- app/seeds/businesses.py
+++ app/seeds/businesses.py
@@ -15,8 +15,8 @@ def seed_businesses():
         zipcode=85251,
         phone_number="(480) 970-7682",
         website="https://cafemonarch.com/",
-        lat="33.4936419",
-        lng="-111.931427",
+        lat=33.4936419,
+        lng=-111.931427,
         price=4,
         hours_of_operation="M: 5pm-10pm, T : 5pm-10pm, W: 5pm-10pm, R : 5pm-10pm, F : 5pm-10pm, S : 5pm-10pm, S  : Closed"
     )
@@ -31,8 +31,8 @@ def seed_businesses():
         zipcode=96779,
         phone_number="(808) 579-8488",
         website="https://www.mamasfishhouse.com/",
-        lat="20.928969",
-        lng="-156.367025",
+        lat=20.928969,
+        lng=-156.367025,
         price=4,
         hours_of_operation="M: 11am-9pm, T : 11am-9pm, W: 11am-9pm, R : 11am-9pm, F : 11am-9pm, S : 11am-9pm, S  : 11am-9pm"
     )
@@ -47,8 +47,8 @@ def seed_businesses():
         zipcode=29401,
         phone_number="(843) 577-2500",
         website="https://huskrestaurant.com/charleston/",
-        lat="32.778057",
-        lng="-79.932141",
+        lat=32.778057,
+        lng=-79.932141,
         price=3,
         hours_of_operation="M: 11am-10pm, T : 11am-10pm, W: 11am--10pm, R : 11am-10pm, F : 11am-10pm, S : 10am-10pm, S  : 10am-2pm"
     )
@@ -63,8 +63,8 @@ def seed_businesses():
         zipcode=60607,
         phone_number="(312) 492-6262",
         website="https://www.girlandthegoat.com/",
-        lat="41.884164",
-        lng="-87.647940",
+        lat=41.884164,
+        lng=-87.647940,
         price=3,
         hours_of_operation="M: 4pm-11pm, T : 4pm-11pm, W: 4pm-11pm, R : 4pm-11pm, F : 4pm-12am, S : 4pm-12am, S  : 4pm-10pm"
     )
@@ -78,8 +78,8 @@ def seed_businesses():
         zipcode=70130,
         phone_number="(504) 899-8221",
         website="https://www.commanderspalace.com/",
-        lat="29.928772",
-        lng="-90.084193",
+        lat=29.928772,
+        lng=-90.084193,
         price=4,
         hours_of_operation="M: 11:30am-9pm, T : 11:30-9pm, W: 11:30am-9pm, R : 11:30am-9pm, F : 11:30am-10pm, S : 10am-10pm, S  : 10am-9pm"
     )
@@ -110,8 +110,8 @@ def seed_businesses():
         zipcode=98109,
         phone_number="(206) 283-3313",
         website="https://canlis.com/",
-        lat="47.643035",
-        lng="-122.346828",
+        lat=47.643035,
+        lng=-122.346828,
         price=4,
         hours_of_operation="M: 5pm-9pm, T : 5pm-9pm, W: 5pm-9pm, R : 5pm-9pm, F : 5pm-10pm, S : 5pm-10pm, S  : 5pm-9pm"
     )
@@ -126,8 +126,8 @@ def seed_businesses():
         zipcode=94102,
         phone_number="(415) 552-2522",
         website="https://zunicafe.com/",
-        lat="37.773623",
-        lng="-122.421663",
+        lat=37.773623,
+        lng=-122.421663,
         price=3,
         hours_of_operation="M: 11:30am-11pm, T : 11:30am-11pm, W: 11:30am-11pm, R : 11:30am-11pm, F : 11:30am-11pm, S : 11:30am-11pm, S  : 11:30am-11pm"
     )
@@ -142,8 +142,8 @@ def seed_businesses():
         zipcode=37203,
         phone_number="(615) 678-4794",
         website="https://hattieb.com/",
-        lat="36.151452",
-        lng="-86.79659",
+        lat=36.151452,
+        lng=-86.79659,
         price=2,
         hours_of_operation="M: 11am-9pm, T : 11am-9pm, W: 11am-9pm, R : 11am-9pm, F : 11am-10pm, S : 11am-10pm, S  : 11am-4pm"
     )
@@ -158,8 +158,8 @@ def seed_businesses():
         zipcode=33131,
         phone_number="(305) 374-4500",
         website="https://www.thecapitalgrille.com/home",
-        lat="25.769182",
-        lng="-80.190424",
+        lat=25.769182,
+        lng=-80.190424,
         price=3,
         hours_of_operation="M: 11:30am-9pm, T : 11:30am-9pm, W: 11:30am-9pm, R : 11:30am-9pm, F : 11:30am-10pm, S : 5pm-10pm, S  : 5pm-9pm"
     )
@@ -174,8 +174,8 @@ def seed_businesses():
         zipcode=60614,
         phone_number="(312) 867-0110",
         website="https://www.alinearestaurant.com/",
-        lat="41.913420",
-        lng="-87.648047",
+        lat=41.913420,
+        lng=-87.648047,
         price=4,
         hours_of_operation="M: Closed, T : Closed, W: 5pm-9pm, R : 5pm-9pm, F : 5pm-9pm, S : 5pm-9pm, S  : Closed"
     )
@@ -190,8 +190,8 @@ def seed_businesses():
         zipcode=10591,
         phone_number="(914) 366-9600",
         website="https://www.bluehillfarm.com/food/blue-hill-stone-barns",
-        lat="41.104209",
-        lng="-73.828575",
+        lat=41.104209,
+        lng=-73.828575,
         price=4,
         hours_of_operation="M: 5pm-9:30pm, T : 5pm-9:30pm, W: 5pm-9:30pm, R : 5pm-9:30pm, F : 5pm-9:30pm, S : 5pm-9:30pm, S  : 5pm-9:30pm"
     )
@@ -206,8 +206,8 @@ def seed_businesses():
         zipcode=55401,
         phone_number="(612) 333-3837",
         website="http://www.barlagrassa.com/",
-        lat="44.989307",
-        lng="-93.278677",
+        lat=44.989307,
+        lng=-93.278677,
         price=2,
         hours_of_operation="M: 4pm-9pm, T : 4pm-9pm, W: 4pm-9pm, R : 4pm-9pm, F : 4pm-9pm, S : 4pm-9pm, S  : 4pm-9pm"
     )
@@ -222,8 +222,8 @@ def seed_businesses():
         zipcode=94133,
         phone_number="(415) 393-9000",
         website="https://coirestaurant.com/",
-        lat="37.798159",
-        lng="-122.403377",
+        lat=37.798159,
+        lng=-122.403377,
         price=4,
         hours_of_operation="W: 5:30pm-8:30pm, R : 5:30pm-8:30pm, F : 5:30pm-8:30pm, S : 5:30pm-8:30pm, S  : 5:30pm-8:30pm, M: Closed, T : Closed"
     )
@@ -238,8 +238,8 @@ def seed_businesses():
         zipcode=90038,
         phone_number="(323) 297-0100",
         website="https://www.osteriamozza.com/",
-        lat="34.083222",
-        lng="-118.338960",
+        lat=34.083222,
+        lng=-118.338960,
         price=3,
         hours_of_operation="M: 5pm-10pm, T : 5pm-10pm, W: 5pm-10pm, R : 5pm-10pm, F : 5pm-10pm, S : 5pm-10pm, S  : 5pm-10pm"
     )
